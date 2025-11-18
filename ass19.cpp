4e4518ec0746b5d831c69fe8ff1b5da070e3df71
Shannon <sestumpf@gmail.com>
2025-11-18 15:58:38 -0800

    added ass19

---
+++ ass19.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
+
+void gross_pay_calculator(){
+    cout << "Enter total hours worked:  " <<endl;
+    const gross_pay_formula = hours * payrate;
+}
